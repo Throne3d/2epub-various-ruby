51932b375642768fe91def1dee93350f747d6473
Edward Jones <throne3d@gmail.com>
2016-05-29 16:47:25 +0100

    Update rainbow link

--- handlers_outputs.rb
+++ handlers_outputs.rb
@@ -402,7 +402,7 @@
           LOG.info "[/list]"
         end
       end
-      LOG.info "[url=http://alicorn.elcenia.com/board/viewtopic.php?f=12&t=191#p4841]Official moiety list[/url] (see [url=http://alicorn.elcenia.com/board/viewtopic.php?f=6&t=124&start=1180#p24135]bottom of this[/url] for rainbow)"
+      LOG.info "[url=http://alicorn.elcenia.com/board/viewtopic.php?f=12&t=191#p4841]Official moiety list[/url] ([url=http://alicorn.elcenia.com/board/viewtopic.php?f=12&t=191&p=24749#p24749]rainbow[/url])"
       
       done_msg = false
       chapter_list.each do |chapter|
