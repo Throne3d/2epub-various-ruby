8770f2bb9f0f7aca9ee52ea4bd42baab792294c5
Edward Jones <throne3d@gmail.com>
2016-07-28 18:32:10 +0100

    Profile on error/interrupt, save to directory

--- prof_do_various.rb
+++ prof_do_various.rb
@@ -3,11 +3,23 @@ require 'ruby-prof'
 $LOAD_PATH << '.'
 require 'do_various'
 
-RubyProf.start
-if __FILE__ == $0
-  main(ARGV)
+def profile_bit(args=ARGV)
+  RubyProf.start
+  begin
+    main(ARGV)
+  rescue StandardError, Interrupt => e
+    yield RubyProf.stop
+    raise e
+  end
+  yield RubyProf.stop
 end
-result = RubyProf.stop
-result.eliminate_methods!([/Object#try!?/, /Integer#upto/, /Kernel#public_send/])
-printer = RubyProf::MultiPrinter.new(result)
-printer.print(:path => '.', :profile => 'profile', :min_percent => 0.05)
+
+if __FILE__ == $0
+  profile_bit(ARGV) do |result|
+    result.eliminate_methods!([/Object#try!?/, /Integer#upto/, /Kernel#public_send/])
+    printer = RubyProf::MultiPrinter.new(result)
+    directory = "prof-" + (ARGV.is_a?(Array) ? ARGV * '_' : ARGV).gsub(/\W+/, '_')
+    FileUtils.mkdir directory unless File.directory?(directory)
+    printer.print(:path => directory, :profile => 'profile', :min_percent => 0.05)
+  end
+end
