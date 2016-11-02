8a317786bbe0e478ce5f5b199c739cda839cfab9
Edward Jones <throne3d@gmail.com>
2016-11-02 12:42:23 +0000

    In the 'toc' process, import chapters just once

--- do_various.rb
+++ do_various.rb
@@ -159,7 +159,7 @@ def main(*args)
 
     group_handler = group_handler.first
 
-    handler = group_handler.new(group: group, chapter_list: chapter_list)
+    handler = group_handler.new(group: group, chapter_list: chapter_list, old_chapter_list: data)
     chapter_list = handler.toc_to_chapterlist(fic_toc_url: fic_toc_url) do |chapter|
       LOG.info chapter.to_s
     end
