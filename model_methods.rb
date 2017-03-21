aa39515843032d08b2cd097b263e7bc62cf0982b
Edward Jones <throne3d@gmail.com>
2017-03-21 19:45:24 +0000

    Cleanup model_methods

--- model_methods.rb
+++ model_methods.rb
@@ -2,10 +2,13 @@
   require 'fileutils'
   require 'open-uri'
   require 'open_uri_redirections'
+
   class FileLogIO
+    attr_reader :file
     def initialize(defaultFile=nil)
-      FileUtils::mkdir_p(File.dirname(defaultFile)) if defaultFile
-      @file = File.open(defaultFile, 'a+') unless defaultFile.nil?
+      return unless defaultFile
+      FileUtils::mkdir_p(File.dirname(defaultFile))
+      @file = File.open(defaultFile, 'a+')
     end
 
     def file=(filename)
@@ -15,29 +18,22 @@
       @file.sync = true
     end
 
-    def file
-      @file
-    end
-
     def set_output_params(process, group=nil)
-      self.file = "logs/" + Time.now.strftime("%Y-%m-%d %H %M ") + "#{process}" + (group.nil? ? "" : "_#{group}") + ".log"
+      self.file = 'logs/' + Time.now.strftime('%Y-%m-%d %H %M ') + " #{process}" + (group.nil? ? '' : "_#{group}") + '.log'
     end
 
-    def write(data)
-      @file.write(data) if @file
-    end
-
-    def close
-      @file.close if @file
-    end
+    def write(data); @file.try(:write, data); end
+    def close; @file.try(:close); end
   end
 
   DEBUGGING = false
+
   CONSOLE = Logger.new(STDOUT)
-  CONSOLE.formatter = proc { |severity, datetime, progname, msg|
+  CONSOLE.formatter = proc { |_severity, _datetime, _progname, msg|
     "#{msg}\n"
   }
   CONSOLE.datetime_format = "%Y-%m-%d %H:%M:%S"
+
   OUTFILE = FileLogIO.new("logs/default.log")
   FILELOG = Logger.new(OUTFILE)
   FILELOG.datetime_format = "%Y-%m-%d %H:%M:%S"
@@ -98,7 +94,9 @@
     temp_starlight: [:temp_starlight, :starlight],
     report: [:report, :daily]
   }
+
   FIC_SHOW_AUTHORS = [:glowfic, :constellation, :sandbox, :marri, :peterverse, :maggie, :throne, :lintamande, :test, :report, :mwf_leaf, :mwf_lioncourt]
+
   FIC_TOCS = {
     #Continuities
     effulgence: "http://edgeofyourseat.dreamwidth.org/2121.html?style=site",
@@ -132,6 +130,7 @@
     temp_starlight: "https://alicornutopia.dreamwidth.org/29069.html",
     report: "https://glowfic.com/boards/"
   }
+
   FIC_AUTHORSTRINGS = {
     effulgence: "Alicorn & Kappa",
     incandescence: "Alicorn & Aestrix",
@@ -160,6 +159,7 @@
     temp_starlight: "Alicorn & Pedro"
   }
   FIC_AUTHORSTRINGS.default = "Unknown"
+
   FIC_NAMESTRINGS = {
     effulgence: "Effulgence",
     incandescence: "Incandescence",
@@ -191,8 +191,7 @@
 
   def is_huge_cannot_dirty(group)
     group = group.group if group.is_a?(Chapters)
-    return true if group == :sandbox || group == :constellation
-    false
+    group == :sandbox || group == :constellation
   end
 
   def date_display(date, strf="%Y-%m-%d %H:%M")
@@ -201,20 +200,17 @@
 
   def get_page_location(page_url, options={})
     standardize_params(options)
-    where = options.key?(:where) ? options[:where] : "web_cache"
+    where = options[:where] || "web_cache"
 
     uri = URI.parse(page_url)
-    return nil unless uri and (uri.scheme == "http" or uri.scheme == "https")
+    return unless uri && (uri.scheme == "http" || uri.scheme == "https")
 
-    uri_query = nil
-    unless uri.query.nil?
-      uri_query = sort_query(uri.query)
-    end
+    uri_query = sort_query(uri.query)
     uri_host = uri.host
     uri_path = uri.path
     uri_folder = (uri_path[-1] == '/') ? uri_path[0..-2] : File.dirname(uri_path)
-    uri_file = uri_path.sub(uri_folder + "/", "")
-    uri_file = "index" if (uri_file.nil? or uri_file.empty?)
+    uri_file = uri_path.sub(uri_folder + '/', '')
+    uri_file = "index" if (uri_file.nil? || uri_file.empty?)
     uri_file += "~QMARK~#{uri_query}" unless uri_query.nil?
 
     save_path = File.join(where, uri_host, uri_folder, uri_file)
@@ -225,22 +221,20 @@
   end
 
   def download_file(file_url, options={})
-    standardize_params(options)
-    headers = options[:headers] if options.key?(:headers)
-    save_path = options[:save_path] if options.key?(:save_path)
-    retries = 3
-
     LOG.debug "download_file('#{file_url}', #{options})"
 
-    save_path ||= get_page_location(file_url, options)
+    standardize_params(options)
+    headers = options[:headers] || {}
+    save_path = options[:save_path] || get_page_location(file_url, options)
+
     save_folder = File.dirname(save_path)
     FileUtils::mkdir_p save_folder
 
+    retries = 3
     success = false
     has_retried = false
     begin
-      param_hash = {:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE, :allow_redirections => :all}
-      param_hash.merge!(headers) if headers
+      param_hash = {:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE, :allow_redirections => :all}.merge(headers)
       open(file_url, param_hash) do |webpage|
         open(save_path, 'w') do |file|
           file.write webpage.read
@@ -256,90 +250,87 @@
       has_retried = true
       retry if retries >= 0
     end
-    LOG.debug "Downloaded page" if success
-    LOG.info "Successfully loaded file (#{file_url})." if has_retried && success
-    LOG.error "Failed to load page (#{file_url})" unless success
 
-    return save_path if success
+    unless success
+      LOG.error "Failed to load page (#{file_url})"
+      return
+    end
+
+    LOG.debug "Downloaded page"
+    LOG.info "Successfully loaded file (#{file_url})." if has_retried
+    save_path
   end
-  def get_page(file_url, options={})
-    standardize_params(options)
-    replace = options.key?(:replace) ? options[:replace] : false
-    save_path = options[:save_path] if options.key?(:save_path)
 
+  def get_page(file_url, options={})
     LOG.debug "get_page('#{file_url}', #{options})"
 
-    save_path ||= get_page_location(file_url, options)
+    standardize_params(options)
+    replace = options[:replace]
+    save_path = options[:save_path] || get_page_location(file_url, options)
 
     return save_path if File.file?(save_path) && !replace
-    return download_file(file_url, options)
+    download_file(file_url, options)
   end
+
   def get_page_data(page_url, options={})
-    standardize_params(options)
     LOG.debug "get_page_data('#{page_url}', #{options})"
+    standardize_params(options)
     file_path = get_page(page_url, options)
 
-    if !File.file?(file_path)
-      return nil
-    end
+    return unless File.file?(file_path)
 
-    data = ""
-    open(file_path, 'r') do |file|
-      data = file.read
-    end
+    data = ''
+    open(file_path, 'r') { |file| data = file.read }
     data
   end
 
   BLOCK_LEVELS = [:address, :article, :aside, :blockquote, :canvas, :dd, :div, :dl, :fieldset, :figcaption, :figure, :footer, :form, :h1, :h2, :h3, :h4, :h5, :h6, :header, :hgroup, :hr, :li, :main, :nav, :noscript, :ol, :output, :p, :pre, :section, :table, :tfoot, :ul, :video, :br]
   def get_text_on_line(node, options={})
     standardize_params(options)
-    raise(ArgumentError, "Invalid parameter combo: :after and :forward") if options.key?(:after) and options.key?(:forward)
-    raise(ArgumentError, "Invalid parameter combo: :before and :backward") if options.key?(:before) and options.key?(:backward)
-    options = {} unless options
+    raise(ArgumentError, "Invalid parameter combo: :after and :forward") if options.key?(:after) && options.key?(:forward)
+    raise(ArgumentError, "Invalid parameter combo: :before and :backward") if options.key?(:before) && options.key?(:backward)
 
-    stop_at = []
-    stop_at = options[:stop_at] if options.key?(:stop_at)
-    stop_at = [stop_at] if not stop_at.is_a?(Array)
+    stop_at = options[:stop_at] || []
+    stop_at = [stop_at] unless stop_at.is_a?(Array)
 
     forward = true
-    forward = options[:forward] if options.key?(:forward)
-    forward = options[:after] if options.key?(:after)
+    forward = options[:forward] || options[:after] if options.key?(:forward) || options.key?(:after)
 
     backward = true
-    backward = options[:backward] if options.key?(:backward)
-    backward = options[:before] if options.key?(:before)
+    backward = options[:backward] || options[:before] if options.key?(:backward) || options.key?(:before)
 
-    include_node = true
-    include_node = options[:include_node] if options.key?(:include_node)
+    include_node = options.key?(:include_node) ? options[:include_node] : true
 
-    text = ""
+    text = ''
     text = node.text if include_node
+
     previous_element = node.previous
-    while backward and previous_element and not BLOCK_LEVELS.include?(previous_element.name) and not BLOCK_LEVELS.include?(previous_element.name.to_sym) and not stop_at.include?(previous_element.name) and not stop_at.include?(previous_element.name.to_sym)
+    while backward && previous_element && !BLOCK_LEVELS.include?(previous_element.name) && !BLOCK_LEVELS.include?(previous_element.name.to_sym) && !stop_at.include?(previous_element.name) && !stop_at.include?(previous_element.name.to_sym)
       text = previous_element.text + text
       previous_element = previous_element.previous
     end
+
     next_element = node.next
-    while forward and next_element and not BLOCK_LEVELS.include?(next_element.name) and not BLOCK_LEVELS.include?(next_element.name.to_sym) and not stop_at.include?(next_element.name) and not stop_at.include?(next_element.name.to_sym)
+    while forward && next_element && !BLOCK_LEVELS.include?(next_element.name) && !BLOCK_LEVELS.include?(next_element.name.to_sym) && !stop_at.include?(next_element.name) && !stop_at.include?(next_element.name.to_sym)
       text = text + next_element.text
       next_element = next_element.next
     end
+
     text
   end
 
   def standardize_chapter_url(url)
-    uri = URI.parse(url)
     url = url.sub('glowfic.herokuapp.com', 'glowfic.com')
-    if uri.host.end_with?("dreamwidth.org")
-      uri.fragment = nil
+    uri = URI.parse(url)
+    uri.fragment = nil
+    if uri.host.end_with?('dreamwidth.org')
       thread = get_url_param(url, "thread")
       params = {style: :site}
-      params[:thread] = thread unless thread.nil? or thread.empty?
-      set_url_params(clear_url_params(uri.to_s), params)
-    elsif uri.host.end_with?("vast-journey-9935.herokuapp.com") || uri.host.end_with?("glowfic.com")
-      uri.fragment = nil
+      params[:thread] = thread unless thread.blank?
+      return set_url_params(clear_url_params(uri.to_s), params)
+    elsif uri.host.end_with?('vast-journey-9935.herokuapp.com') || uri.host.end_with?('glowfic.com')
       params = {per_page: :all}
-      set_url_params(clear_url_params(uri.to_s), params)
+      return set_url_params(clear_url_params(uri.to_s), params)
     else
       url
     end
@@ -347,16 +338,13 @@
 
   def standardize_params(params={})
     params.keys.each do |key|
-      if key.is_a? String
-        params[key.to_sym] = params[key]
-        params.delete(key)
-      end
+      params[key.to_sym] = params.delete(key) if key.is_a?(String)
     end
+    params
   end
 
   def sort_query(query)
-    return nil if query.nil?
-    return nil if query.empty?
+    return if query.blank?
 
     query_hash = CGI::parse(query)
     sorted_keys = query_hash.keys.sort
@@ -367,64 +355,64 @@
     end
     sorted_query = URI.encode_www_form(sorted_list)
 
-    return nil if sorted_query.empty?
+    return if sorted_query.empty?
     sorted_query
   end
 
   def set_url_params(chapter_url, params={})
     uri = URI(chapter_url)
-    uri_query = (uri.query or "")
+    uri_query = uri.query || ''
     paramstr = URI.encode_www_form(params)
-    uri_query += "&" unless uri_query.empty? or paramstr.empty?
+    uri_query += '&' unless uri_query.empty? || paramstr.empty?
     uri_query += paramstr
 
-    uri_query = sort_query(uri_query)
-    uri.query = uri_query
+    uri.query = sort_query(uri_query)
     uri.to_s
   end
+
   def clear_url_params(chapter_url)
     uri = URI(chapter_url)
-    uri.query = ""
+    uri.query = ''
     uri.to_s
   end
+
   def get_url_params_for(chapter_url, param_name)
-    return nil if chapter_url.nil? or chapter_url.empty?
+    return if chapter_url.blank?
     uri = URI(chapter_url)
-    return [] unless uri.query and not uri.query.empty?
+    return [] unless uri.query.present?
     query_hash = CGI::parse(uri.query)
     return [] unless query_hash.key?(param_name)
-    return query_hash[param_name]
+    query_hash[param_name]
   end
   def get_url_param(chapter_url, param_name, default=nil)
-    return default if chapter_url.nil? or chapter_url.empty?
+    return default if chapter_url.blank?
     params = get_url_params_for(chapter_url, param_name)
-    return default if params.empty?
-    return params.first
+    params.first || default
   end
 
   def oldify_chapters_data(group, options={})
-    old_where = options.key?(:where) ? options[:where] : (options.key?(:old) ? options[:old] : (options.key?(:old_where) ? options[:old_where] : "web_cache/chapterdetails_#{group}.txt"))
-    new_where = options.key?(:new) ? options[:new] : (options.key?(:new_where) ? options[:new_where] : "")
+    old_where = options[:where] || options[:old] || options[:old_where] || "web_cache/chapterdetails_#{group}.txt"
+    new_where = options[:new] || options[:new_where] || ''
 
     old_where = old_where.gsub("\\", "/")
     new_where = new_where.gsub("\\", "/")
-    if new_where == ""
-      where_bits = old_where.split("/")
+    if new_where.empty?
+      where_bits = old_where.split('/')
       where_bits[-1] = "old_" + where_bits.last
-      new_where = where_bits * "/"
+      new_where = where_bits * '/'
     end
 
-    return if not File.file?(old_where)
-    File.open(old_where, "r") do |old|
-      File.open(new_where, "w") do |new|
-        new.write old.read
+    return unless File.file?(old_where)
+    File.open(old_where, "r") do |oldf|
+      File.open(new_where, "w") do |newf|
+        newf.write oldf.read
       end
     end
   end
 
   def get_chapters_data(group, options={})
-    where = (options.key?(:where)) ? options[:where] : "web_cache/chapterdetails_#{group}.txt"
-    trash_messages = (options.key?(:trash_messages)) ? options[:trash_messages] : false
+    where = options[:where] || "web_cache/chapterdetails_#{group}.txt"
+    trash_messages = options[:trash_messages] || false
 
     chapterRep = GlowficEpub::Chapters.new(group: group, trash_messages: trash_messages)
 
@@ -432,20 +420,16 @@
     File.open(where, "r") do |f|
       chapterRep.from_json! f.read.scrub
     end
-    return chapterRep
+    chapterRep
   end
+
   def set_chapters_data(chapters, group, others={})
     standardize_params(others)
-    where = others.key?(:where) ? others[:where] : ""
-    old = others.key?(:old) ? others[:old] : false
-    where = unless where == ""
-      where
-    else
-      if old
-        "web_cache/old_chapterdetails_#{group}.txt"
-      else
-        "web_cache/chapterdetails_#{group}.txt"
-      end
+    old = others[:old] || false
+    where = others[:where] || ''
+
+    if where.empty?
+      where = 'web_cache/' + (old ? 'old_' : '') + "chapterdetails_#{group}.txt"
     end
     if chapters.is_a?(Array)
       chapterRep = GlowficEpub::Chapters.new
@@ -455,45 +439,41 @@
       chapters = chapterRep
     end
     temp = chapters.to_json
-    File.open(where, "w") do |f|
+    File.open(where, 'w') do |f|
       f.write(temp)
     end
-    LOG.debug "Saved data for group: #{group}" + (others.empty? ? "" : " (#{others.inspect})")
+    LOG.debug "Saved data for group: #{group}" + (others.empty? ? '' : " (#{others.inspect})")
   end
 
   def clear_old_data(group=nil)
-    return @temp_data.delete(group) if group
-    @temp_data = {}
+    @temp_data = {} if group.nil?
+    @temp_data.delete(group)
   end
+
   def get_old_data(group)
     @temp_data ||= {}
-    @temp_data[group] = get_chapters_data(group) unless @temp_data.key?(group)
-    @temp_data[group]
+    @temp_data[group] ||= get_chapters_data(group)
   end
+
   def get_prev_chapter_detail(group, others={})
     if group.is_a?(Hash) && others.nil?
       others = group
-      group = nil
-      group = others[:group] if others.key?(:group)
+      group = others[:group]
     end
     if others.is_a?(Hash)
       detail = others[:detail]
-      remove_empty = others[:remove_empty] or others[:reject_empty]
       only_present = others[:only_present]
       chapters = others[:chapters] || others[:chapter_list]
       group = chapters.group if chapters.present? && group.blank?
     else
       detail = others
     end
-    remove_empty ||= false
-
     chapters ||= get_old_data(group)
 
     prev_detail = {}
     chapters.each do |chapter|
       prev_detail[chapter.url] = chapter.try(detail)
     end
-    prev_detail.reject! {|key, value| value.nil? or value.empty?} if remove_empty
     prev_detail.select! {|key, value| value.present?} if only_present
     prev_detail
   end
