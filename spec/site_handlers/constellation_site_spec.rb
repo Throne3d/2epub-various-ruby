b26558b36bf3708e0316ed6897ecf2ffb0f07598
Edward Jones <throne3d@gmail.com>
2017-12-21 18:16:57 +0000

    Start testing Constellation SiteHandler

--- spec/site_handlers/constellation_site_spec.rb
+++ spec/site_handlers/constellation_site_spec.rb
@@ -1,12 +1,48 @@
 RSpec.describe GlowficSiteHandlers::ConstellationHandler do
+  Handler = GlowficSiteHandlers::ConstellationHandler
+  let(:handler) { Handler.new }
+
   describe "handles?" do
+    it "does not handle nil" do
+      expect(Handler.handles?(nil)).to be_falsy
+    end
+
     it "handles constellation chapters"
-    it "does not handle other chapters"
+    it "does not handle non-constellation chapters"
+
+    it "does not handle blank URLs" do
+      expect(Handler.handles?('')).to be_falsy
+    end
+
+    it "handles constellation URLs" do
+      expect(Handler.handles?('https://glowfic.com/posts/1')).to eq(true)
+      expect(Handler.handles?('https://www.glowfic.com/posts/1')).to eq(true)
+    end
+
+    it "does not handle non-constellation URLs" do
+      expect(Handler.handles?('https://alicornutopia.dreamwidth.org/1640.html')).to eq(false)
+    end
   end
 
   describe "#get_permalink_for" do
-    it "gets right URL for post"
-    it "gets right URL for reply"
+    def create_entry(**kwargs)
+      Entry.new(**kwargs)
+    end
+
+    def create_reply(**kwargs)
+      Comment.new(**kwargs)
+    end
+
+    it "gets right URL for post" do
+      entry = create_entry(content: '', chapter: nil, id: '9876')
+      expect(handler.get_permalink_for(entry)).to eq('https://glowfic.com/posts/9876')
+    end
+
+    it "gets right URL for reply" do
+      reply = create_reply(content: '', chapter: nil, id: '9876')
+      expect(handler.get_permalink_for(reply)).to eq('https://glowfic.com/replies/9876#reply-9876')
+    end
+
     it "does something good with neither"
   end
 
@@ -14,11 +50,15 @@ RSpec.describe GlowficSiteHandlers::ConstellationHandler do
     it "uses get_some because for some reason" # TODO: why? what do these names mean?
   end
 
-  describe "#get_some" do
+  describe "#get_flat_page_for" do
     it "uses flat mode" # FIXME: do this
     it "does not proceed if it doesn't handle the chapter"
     it "downloads each relevant page"
   end
+
+  describe "#check_webpage_accords_with_disk"
+
+  describe "#check_cachepage_accords_with_disk"
 
   describe "#get_updated" do
     it "checks last few pages rather than using a ton of bandwidth and processing time for unchanged files"
